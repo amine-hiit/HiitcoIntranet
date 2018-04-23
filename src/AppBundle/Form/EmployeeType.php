afe480a64a774f25481db08cebb718cda6a948f6
amine-hiit <amine.j@hiitconsulting.com>
2018-04-24 00:30:01 +0100

    modify employeeType.php

--- src/AppBundle/Form/EmployeeType.php
+++ src/AppBundle/Form/EmployeeType.php
@@ -2,9 +2,11 @@
 
 namespace AppBundle\Form;
 
+use AppBundle\Entity\Experience;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\DateType;
 use Symfony\Component\Form\Extension\Core\Type\CollectionType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
@@ -19,15 +21,25 @@ class EmployeeType extends AbstractType
         $builder
             ->add('firstName')
             ->add('lastName')
-            ->add('birthday')
+
+            ->add('birthday',DateType::class, array('format' => 'MM/dd/yyyy',
+                'widget' => 'single_text'))
             ->add('maritalStatus')
             ->add('dependentChild')
             ->add('cnssNumber')
             ->add('phoneNumber')
             ->add('address')
-            ->add('startDate')
-            ->add('currentPosition')
+
+            ->add('startDate',DateType::class, array(
+                'format' => 'MM/dd/yyyy',
+                'widget' => 'single_text',
+                'required' => false))
+            ->add('avatar',     AvatarType::class)
+
+            ->add('currentPosition',TextType::class, array(
+                'required' => false))
             ->add('status')
+
             ->add('employee_formations', CollectionType::class, array(
                 // each entry in the array will be an "email" field
                 'entry_type' => EmployeeFormationType::class,
@@ -36,13 +48,23 @@ class EmployeeType extends AbstractType
                 'allow_delete' => true,
                 'prototype' => true,
                 'prototype_name' => '__name__',
-                ));
+            ))
+            ->add('experiences', CollectionType::class, array(
+                // each entry in the array will be an "email" field
+                'entry_type' => ExperienceType::class,
+                'allow_add' => true,
+                'delete_empty' => true,
+                'allow_delete' => true,
+                'prototype' => true,
+            ));
 
-            //->add('employeeformation', EmployeeFormationType::class);
+
+
+        //->add('employeeformation', EmployeeFormationType::class);
 
     }/**
-     * {@inheritdoc}
-     */
+ * {@inheritdoc}
+ */
     public function configureOptions(OptionsResolver $resolver)
     {
         $resolver->setDefaults(array(
